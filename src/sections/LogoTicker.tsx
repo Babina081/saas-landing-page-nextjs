a58deabba5a32d027f01a74f656493662a3d6f05
Babina081 <babitamoo333@gmail.com>
2024-08-13 09:54:50 +0545

    Add: logoticker section in main page

---
+++ src/sections/LogoTicker.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+
+const LogoTicker = () => {
+  return (
+    <div className="py-8 bg-white md:py-12">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image
+              src={acmeLogo}
+              alt="acme logo"
+              className="logo-ticker-image"
+            ></Image>
+            <Image
+              src={quantumLogo}
+              alt="Quantum logo"
+              className="logo-ticker-image"
+            ></Image>
+            <Image
+              src={echoLogo}
+              alt="Echo logo"
+              className="logo-ticker-image"
+            ></Image>
+            <Image
+              src={celestialLogo}
+              alt="Celestial logo"
+              className="logo-ticker-image"
+            ></Image>
+            <Image
+              src={pulseLogo}
+              alt="Pulse logo"
+              className="logo-ticker-image"
+            ></Image>
+            <Image
+              src={apexLogo}
+              alt="Apex logo"
+              className="logo-ticker-image"
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LogoTicker;
