7f6aa82e34c596bd77520077966a4a83e3ebb33c
Babina081 <babitamoo333@gmail.com>
2024-08-14 12:36:25 +0545

    Add: animation to pricing section

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
 import CheckIcon from "@/assets/check.svg";
 import { twMerge } from "tailwind-merge";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const pricingTiers = [
   {
@@ -94,9 +96,18 @@ const Pricing = () => {
                   </h3>
                   {popular === true && (
                     <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                      <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                      <motion.span
+                        className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text font-medium"
+                        animate={{ backgroundPositionX: "100%" }}
+                        transition={{
+                          repeat: Infinity,
+                          ease: "linear",
+                          repeatType: "loop",
+                          duration: 1,
+                        }}
+                      >
                         Popular
-                      </span>
+                      </motion.span>
                     </div>
                   )}
                 </div>
