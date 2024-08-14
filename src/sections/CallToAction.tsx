4a29e9689b1812251b0ec0f11960e1d0fa046e36
Babina081 <babitamoo333@gmail.com>
2024-08-14 13:47:52 +0545

    Add: animation to call to action section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import ArrowRight from "@/assets/arrow-right.svg";
 import starImage from "@/assets/star.png";
 import springImage from "@/assets/spring.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const CallToAction = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF]  py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF]  py-24 overflow-x-clip"
+    >
       <div className="container mx-auto">
         <div className="section-heading relative">
           <h2 className="section-title ">Sign up for free today</h2>
@@ -15,18 +27,20 @@ const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
-          <Image
-            src={starImage}
+          <motion.img
+            src={starImage.src}
             alt="star image"
             width={360}
             className="absolute -left-[350px] -top-[137px]"
-          ></Image>
-          <Image
-            src={springImage}
+            style={{ translateY: translateY }}
+          ></motion.img>
+          <motion.img
+            src={springImage.src}
             alt="spring image"
             width={360}
             className="absolute -right-[310px] -top-[19px]"
-          ></Image>
+            style={{ translateY: translateY }}
+          ></motion.img>
         </div>
         <div className="flex gap-2 mt-10 justify-center ">
           <button className="btn btn-primary">Get for free</button>
